746f657423d2278dc1a33dedc6cc2389ec756a36
fatema-17 <202302468@stu.uob.edu.bh>
2026-03-29 21:52:07 +0300

    final solution

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
  */
 function sum(a, b) {
   // TODO: Implement this function.
+  return a+b;
 }
 
 /**
@@ -13,6 +14,7 @@ function sum(a, b) {
  */
 function reverseString(str) {
   // TODO: Implement this function.
+  return str.split("").reverse().join("");
 }
 
 /**
@@ -21,6 +23,8 @@ function reverseString(str) {
  */
 function findLargest(numbers) {
   // TODO: Implement this function.
+  if (numbers.length === 0) return null;
+  return Math.max(...numbers);
 }
 
 /**
@@ -31,6 +35,10 @@ function findLargest(numbers) {
  */
 function isPalindrome(str) {
   // TODO: Implement this function.
+  const cleaned = str.toLowerCase();
+  const reversed = cleaned.split('').reverse().join('');
+  return cleaned === reversed;
+
 }
 
 /**
@@ -39,6 +47,7 @@ function isPalindrome(str) {
  */
 function filterEvenNumbers(numbers) {
   // TODO: Implement this function.
+  return numbers.filter(num => num % 2 === 0);
 }
 
 
